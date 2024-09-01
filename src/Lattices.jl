32dec28677cccb1ac7424f0cd4ed760b28f469fb
Davis Thuillier <dsthuillier@gmail.com>
2024-09-01 10:27:04 -0700

    Added function for calculating BZ of 2D lattices

--- src/Lattices.jl
+++ src/Lattices.jl
@@ -24,6 +24,8 @@ end
 ### Convenience Constructors ###
 Lattice(a1::AbstractVector, a2::AbstractVector) = Lattice(hcat(a1, a2))
 
+### Lattice Methods ###
+Base.:(==)(l1::Lattice, l2::Lattice) = primitives(l1) == primitives(l2)
 primitives(l::Lattice) = l.primitives
 reciprocal_lattice_vectors(l::Lattice) = inv(primitives(l))
 point_group(l::Lattice) = point_groups(lattice_type(l))
@@ -35,17 +37,47 @@ function lattice_type(l::Lattice)
 
     cosθ = abs( dot(a1, a2) / (norm(a1) * norm(a2)) )
 
-    if cosθ == 0.0
+    if cosθ ≈ 0.0
         if norm(a1) == norm(a2)
             return "Square"
         else
             return "Rectangular"
         end
-    elseif cosθ == 0.5
+    elseif cosθ ≈ 0.5
         return "Hexagonal"
     else
         return "Oblique"
     end
 end
+
+function get_bz(l::Lattice)
+    rlv = reciprocal_lattice_vectors(l)
+    b1 = rlv[:, 1]
+    b2 = rlv[:, 2] 
+    
+    vertices = Vector{AbstractArray}(undef, 0)
+
+    get_perpendicular_bisector_intersection(b1, b1 + b2) |> x -> push!(vertices, x)
+    get_perpendicular_bisector_intersection(b1 + b2, b2) |> x -> push!(vertices, x)
+    get_perpendicular_bisector_intersection(b2, b2 - b1) |> x -> push!(vertices, x)
+    get_perpendicular_bisector_intersection(b2 - b1, -b1) |> x -> push!(vertices, x)
+    get_perpendicular_bisector_intersection(-b1, -b1 - b2) |> x -> push!(vertices, x)
+    get_perpendicular_bisector_intersection(-b1 - b2, -b2) |> x -> push!(vertices, x)
+    get_perpendicular_bisector_intersection(-b2, b1 - b2) |> x -> push!(vertices, x)
+    get_perpendicular_bisector_intersection(b1 - b2, b1) |> x -> push!(vertices, x)
+    
+    deleteat!(vertices, vertices .== [[0.0, 0.0]])
+    deleteat!(vertices, vertices .== [[]])
+end
+
+function get_perpendicular_bisector_intersection(v1, v2)
+    V = [v1[2] v2[2]; -v1[1] -v2[1]]
+    det(V) == 0 && return [] # No intersection
+    
+    t = 0.5 * inv(V) * (v2 - v1)
+    return (0.5 + t[1]) * v1
+end
+
+
 
 end # module
--- test/lattice_tests.jl
+++ test/lattice_tests.jl
@@ -1,11 +1,23 @@
+using Ludwig.Lattices
+
 @testset "Lattice Constructors" begin
     a1 = [1.0, 1.0]
     a2 = [1.0, -2.0]
     A  = [1.0 1.0; 1.0 -2.0]
 
-    @test Lattices.primitives(Lattices.Lattice(a1, a2)) == A
+    @test Lattice(a1, a2) == Lattice(A)
 end
 
 @testset "Lattice Types" begin
-    @test Lattices.lattice_type(Lattices.Lattice([1.0 0.0; 0.0 1.0])) == "Square"
+    sqr_lat  = Lattice([1.0 0.0; 0.0 1.0])
+    @show Lattices.get_bz(sqr_lat)
+    @test lattice_type(sqr_lat) == "Square"
+
+    obl_lat = Lattice([1.0 0.0; 0.2 -0.5])
+    @show Lattices.get_bz(obl_lat)
+    @test lattice_type(obl_lat) == "Oblique"
+
+    hex_lat = Lattice([1.0 -0.5; 0.0 sqrt(3)/2.0])
+    @show Lattices.get_bz(hex_lat)
+    @test lattice_type(hex_lat) == "Hexagonal"
 end
