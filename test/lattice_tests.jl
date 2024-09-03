3275433c5cf44b0f1cfa2752ef6df55d3360724d
Davis Thuillier <dsthuillier@gmail.com>
2024-09-03 13:01:56 -0700

    Added test set for mapping back to the BZ

--- test/lattice_tests.jl
+++ test/lattice_tests.jl
@@ -10,19 +10,29 @@ end
 
 @testset "Lattice Types" begin
     obl_lat = Lattice([1.0 0.0; 0.2 -0.5])
-    # @show Lattices.get_bz(obl_lat)
     @test lattice_type(obl_lat) == "Oblique"
 
     sqr_lat = Lattice([1.0 0.0; 0.0 1.0])
-    # @show Lattices.get_bz(sqr_lat)
     @test lattice_type(sqr_lat) == "Square"
 
     rec_lat = Lattice([1.0 0.0; 0.0 2.0])
-    # @show Lattices.get_bz(rec_lat)
     @test lattice_type(rec_lat) == "Rectangular"
 
     hex_lat = Lattice([1.0 -0.5; 0.0 sqrt(3)/2.0])
-    # @show Lattices.get_bz(hex_lat)
     @test lattice_type(hex_lat) == "Hexagonal"
 
+end
+
+@testset "Brillouin Zone Mapping" begin
+    hex_lat = Lattice([1.0 -0.5; 0.0 sqrt(3)/2.0])
+    bz = Lattices.get_bz(hex_lat)
+    rlv = Lattices.reciprocal_lattice_vectors(hex_lat)
+    x_range, y_range = Ludwig.get_bounding_box(bz)
+
+    for i in 1:1000
+        kx = x_range[1] + (x_range[2] - x_range[1]) * Random.rand(Float64)
+        ky = y_range[1] + (y_range[2] - y_range[1]) * Random.rand(Float64)
+        k = Lattices.map_to_bz([kx, ky], rlv)
+        @test Lattices.in_polygon(k, bz)
+    end
 end
