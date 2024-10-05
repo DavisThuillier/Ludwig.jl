607167bc6fffd2e9f9cd7b4d126cc375f0624b63
Davis Thuillier <dsthuillier@gmail.com>
2024-10-04 17:37:18 -0700

    Remove old diagonlization utility

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,4 +1,4 @@
-export f0, map_to_first_bz, get_bands
+export f0, map_to_first_bz, symmetrize
 
 """
         f0(E, T)
@@ -18,26 +18,12 @@ Map a vector `k` to the ``d``-dimensional centered unit cube where ``d`` is the 
 map_to_first_bz(k) = mod.(k .+ 0.5, 1.0) .- 0.5
 
 """
-        get_bands(H, N)
+        symmetrize(L, dV, E, T)
 
-Return an interpolation of the eigenvalues of `H` on a square grid [-0.5, 0.5].
-
-It is assumed that `H` is a function of a vector of length 2 and returns a square matrix.
-`N` is the number of points between -0.5 and 0.5 used for interpolation.
 """
-function get_bands(H::Function, N::Int)
-    n_bands = size(H([0.0, 0.0]))[1] # Number of bands
-    E = Array{Float64}(undef, N, N, n_bands)
-    x = LinRange(-0.5, 0.5, N)
-    for i in 1:N, j in 1:N
-        E[i, j, :] .= eigvals(H([x[i], x[j]]))# Get eigenvalues (bands) of each k-point
-    end
+function symmetrize(L, dV, E, T)
+    fd = f0.(E, T) # Fermi dirac on grid points
+    D = diagm(dV .* fd .* (1 .- fd))
 
-    sitps = Vector{ScaledInterpolation}(undef, n_bands)
-    for i in 1:n_bands
-        itp = interpolate(E[:,:,i], BSpline(Cubic(Line(OnGrid()))))
-        sitps[i] = scale(itp, -0.5:1/(N-1):0.5, -0.5:1/(N-1):0.5)
-    end
-
-    return sitps
+    return 0.5 * (L + inv(D) * L' * D)
 end
