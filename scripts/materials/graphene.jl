16e1933448ca367df43abc98ffbe2deb428f3d39
Davis Thuillier <dsthuillier@gmail.com>
2025-01-06 14:55:16 -0800

    Added chemical potential from doping to function argument for graphene

--- scripts/materials/graphene.jl
+++ scripts/materials/graphene.jl
@@ -2,14 +2,13 @@ const material::String = "graphene"
 
 const a::Float64 = 2.46e-10  # Lattice constant in m
 const t::Float64 = 2.8 # Nearest neighbor hopping in eV
-const μ::Float64 = 0.0
 
-function ε1(k)
+function ε1(k, μ = 0.0)
     return μ - t * sqrt(1 + 4 * cos(pi * k[1])^2 + 4 * cos(pi * k[1]) * cos(pi * sqrt(3) * k[2]) )
 end
 
-function ε2(k)
-    return μ + t * sqrt(1 + 4 * ((cos(k[1]/2.0))^2 + cos(k[1]/2.0) * cos(sqrt(3) * k[2] / 2.0) ))
+function ε2(k, μ = 0.0)
+    return μ + t * sqrt(1 + 4 * cos(pi * k[1])^2 + 4 * cos(pi * k[1]) * cos(pi * sqrt(3) * k[2]) )
 end
 
 orbital_weights(k) = (1 / sqrt(2)) * [1.0 1.0; 1.0 -1.0]
