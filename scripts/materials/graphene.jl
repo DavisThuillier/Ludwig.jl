c79d2da85fce3cbb374684916413aaef2efbd86f
Davis Thuillier <dsthuillier@gmail.com>
2025-02-06 10:09:01 -0800

    Added interband weights to graphene material file

--- scripts/materials/graphene.jl
+++ scripts/materials/graphene.jl
@@ -3,6 +3,10 @@ const material::String = "graphene"
 const a::Float64 = 2.46e-10  # Lattice constant in m
 const t::Float64 = 2.8 # Nearest neighbor hopping in eV
 
+function Δ(k)
+    return exp(-2π * im * k[1]) * (1 + 2 * exp(3π * im * k[1]) * cos(sqrt(3) * π * k[2]))
+end
+
 function ε1(k, μ = 0.0)
     return μ - t * sqrt(1 + 4 * cos(pi * k[1])^2 + 4 * cos(pi * k[1]) * cos(pi * sqrt(3) * k[2]) )
 end
@@ -11,6 +15,26 @@ function ε2(k, μ = 0.0)
     return μ + t * sqrt(1 + 4 * cos(pi * k[1])^2 + 4 * cos(pi * k[1]) * cos(pi * sqrt(3) * k[2]) )
 end
 
-orbital_weights(k) = (1 / sqrt(2)) * [1.0 1.0; 1.0 -1.0]
+function orbital_weights(k)
+    u = Δ(k)
+    u /= abs(u)
 
-bands = [ε1, ε2]
+    return (1 / sqrt(2)) * [-u 1.0; u 1.0]
+end
+
+"""
+    vertex_factor(p1, p2)
+"""
+vertex_pp(p1::Patch, p2::Patch) = dot(p1.w, p2.w)
+
+
+function vertex_pk(p::Patch, k, μ)
+    u = Δ(k)
+    u /= abs(u)
+
+    if μ == 1
+        return (- u * p.w[1] + p.w[2]) / sqrt(2)
+    else
+        return (u * p.w[1] + p.w[2]) / sqrt(2)
+    end
+end
