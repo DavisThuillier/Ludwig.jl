6838928b46a3351e29af423b6393c893fa89e139
Davis Thuillier <dsthuillier@gmail.com>
2025-02-11 17:12:40 -0800

    Consolidated output in mesh container struct and added function for generating circular FS meshes

--- src/mesh/FSMesh.jl
+++ src/mesh/FSMesh.jl
@@ -3,11 +3,14 @@ module FSMesh
 import StaticArrays: SVector, SMatrix
 using LinearAlgebra
 import ..MarchingSquares: Isoline, get_bounding_box, contours, contour_intersection
-import ..Lattices: Lattice, get_ibz, in_polygon, point_group
+import ..Lattices: Lattice, get_ibz, point_group
 import ..Groups: get_matrix_representation
-import ForwardDiff: gradient
+using ..GeometryUtilities
+import ForwardDiff: gradient, derivative
 
 export Patch, VirtualPatch, AbstractPatch
+export Mesh, patches, corners, corner_indices
+export mesh_region, ibz_mesh, bz_mesh, circular_fs_mesh
 
 abstract type AbstractPatch end
 
@@ -57,12 +60,17 @@ Container struct for patches over which to integrate.
 # Fields
 - `patches`: Vector of patches
 - `corners`: Vector of points on patch corners for plotting mesh
+- `corner_inds`: Vector of vector of indices in the `corners` vector corresponding to the corners of each patch in `patches`
 """
 struct Mesh
     patches::Vector{Patch}
     corners::Vector{SVector{2, Float64}} 
     corner_inds::Vector{SVector{4, Int}}
 end
+
+patches(m::Mesh) = m.patches
+corners(m::Mesh) = m.corners
+corner_indices(m::Mesh) = m.corner_inds
 
 ###
 # General IBZ Meshes
@@ -219,41 +227,26 @@ function mesh_region(region, ε, band_index::Int, T, n_levels::Int, n_cuts::Int,
 
     end
 
-    return vec(patches), corners, vec(corner_ids)
+    return Mesh(vec(patches), corners, vec(corner_ids))
 end
 
 mesh_region(region, ε, T, n_levels, n_cuts, N = 1001, α = 6.0) = mesh_region(region, ε, 1, T, n_levels, n_cuts, N, α)
 
-function poly_area(poly, c)
-    A = 0.0
-    N = length(poly)
-    oriented_poly = sort(poly, by = x -> atan(x[2] - c[2], x[1] - c[1]))
-
-    for i in eachindex(oriented_poly)
-        if i == N
-            A += (oriented_poly[i][2] + oriented_poly[1][2]) * (oriented_poly[i][1] - oriented_poly[1][1])
-        else
-            A += (oriented_poly[i][2] + oriented_poly[i+1][2]) * (oriented_poly[i][1] - oriented_poly[i+1][1])
-        end
-    end
-    return A/2
-end
-
 function ibz_mesh(l::Lattice, bands::AbstractVector, T, n_levels::Int, n_cuts::Int, N::Int = 1001, α::Real = 6.0)
     full_patches = Vector{Patch}(undef, 0)
     full_corners = Vector{SVector{2, Float64}}(undef, 0)
-    full_corner_ids = Vector{SVector{4, Int}}
+    full_corner_inds = Vector{SVector{4, Int}}
     ibz = get_ibz(l)
 
     for i in eachindex(bands)
-        patches, corners, corner_ids = mesh_region(ibz, bands[i], i, T, n_levels, n_cuts, N, α)
-        full_patches = vcat(full_patches, patches)
+        mesh = mesh_region(ibz, bands[i], i, T, n_levels, n_cuts, N, α)
+        full_patches = vcat(full_patches, mesh.patches)
         ℓ = length(full_corners)
-        full_corner_ids = vcat(full_corners, map(x -> SVector{4, Int}(x .+ ℓ), corner_ids))
-        full_corners = vcat(full_corners, corners)
+        full_corner_inds = vcat(full_corners, map(x -> SVector{4, Int}(x .+ ℓ), mesh.corner_inds))
+        full_corners = vcat(full_corners, mesh.corners)
     end
 
-    return full_patches, full_corners, full_corner_ids
+    return Mesh(full_patches, full_corners, full_corner_inds)
 end
 
 ibz_mesh(l::Lattice, ε::Function, T, n_levels::Int, n_cuts::Int, N::Int = 1001, α::Real = 6.0) = ibz_mesh(l, [ε], T, n_levels, n_cuts, N, α)
@@ -272,34 +265,114 @@ function bz_mesh(l::Lattice, bands::AbstractVector, T, n_levels::Int, n_cuts::In
 
     full_patches = Matrix{Patch}(undef, n_levels - 1, 0)
     full_corners = Vector{SVector{2, Float64}}(undef, 0)
-    full_corner_ids = Matrix{SVector{4, Int}}(undef, n_levels - 1, 0)
+    full_corner_inds = Matrix{SVector{4, Int}}(undef, n_levels - 1, 0)
 
     for j in eachindex(bands)
-        patches, corners, corner_ids = mesh_region(ibz, bands[j], j, T, n_levels, n_cuts, N, α)
+        mesh = mesh_region(ibz, bands[j], j, T, n_levels, n_cuts, N, α)
         
         for i in θperm
             O = get_matrix_representation(G.elements[i])
             ℓ = length(full_corners)
             if det(O) < 0.0 # Improper rotation
                 full_patches = hcat(full_patches, 
-                    reverse( map(x -> patch_op(x, O), reshape(patches, n_levels - 1, :)), dims = 2)
+                    reverse( map(x -> patch_op(x, O), reshape(mesh.patches, n_levels - 1, :)), dims = 2)
                 )
-                full_corner_ids = hcat(full_corner_ids, reverse( map(x -> SVector{4, Int}(x .+ ℓ), reshape(corner_ids,  n_levels - 1, :)), dims = 2))
+                full_corner_inds = hcat(full_corner_inds, reverse( map(x -> SVector{4, Int}(x .+ ℓ), reshape(mesh.corner_inds,  n_levels - 1, :)), dims = 2))
             else
                 full_patches = hcat(full_patches, 
-                    map(x -> patch_op(x, O), reshape(patches, n_levels - 1, :)) 
+                    map(x -> patch_op(x, O), reshape(mesh.patches, n_levels - 1, :)) 
                 )
-                full_corner_ids = hcat(full_corner_ids, map(x -> SVector{4, Int}(x .+ ℓ), reshape(corner_ids,  n_levels - 1, :)))
+                full_corner_inds = hcat(full_corner_inds, map(x -> SVector{4, Int}(x .+ ℓ), reshape(mesh.corner_inds,  n_levels - 1, :)))
             end
             
             
-            full_corners = vcat(full_corners, map(x -> O*x, corners))
+            full_corners = vcat(full_corners, map(x -> O*x, mesh.corners))
         end
     end
 
-    return vec(full_patches), vec(full_corners), vec(full_corner_ids)
+    return Mesh(vec(full_patches), vec(full_corners), vec(full_corner_inds))
 end
 
 bz_mesh(l::Lattice, ε, T, n_levels::Int, n_cuts::Int, N::Int = 1001, α::Real = 6.0) = bz_mesh(l, [ε], T, n_levels, n_cuts, N, α)
+
+function secant_method(f, x0, x1, maxiter; atol = eps(Float64))
+    x2 = 0.0
+    for i in 1:maxiter
+        x2 = x1 - f(x1) * (x1 - x0) / (f(x1) - f(x0))
+        x0, x1 = x1, x2
+        abs(f(x2)) < atol && break
+    end
+
+    return x2
+end
+
+function circular_fs_mesh(ε, T::Real, n_levels::Int, n_angles::Int, α::Real = 6.0; maxiter = 10000, atol = 1e-10)
+    n_levels = max(3, n_levels) # Enforce minimum of 2 patches in the energy direction
+    n_angles = max(3, n_angles) # Enforce minimum of 2 patches in angular direction
+    Δθ = 2π / (n_angles - 1) 
+    Δε = 2*α*T / (n_levels - 1)
+    
+    # Slice contours to generate patch corners
+    corners = Matrix{SVector{2,Float64}}(undef, n_levels, n_angles)
+    k = Matrix{SVector{2,Float64}}(undef, n_levels-1, n_angles-1)
+    corner_indices = Matrix{SVector{4,Int}}(undef, n_levels-1, n_angles-1)
+    radius = Vector{Float64}(undef, 2*n_levels - 1)
+    
+
+    for i ∈ 1:2*n_levels-1
+        E = (i - 1) * Δε/2 - α*T
+        r0 = i == 1 ? 0.0 : radius[i-1]
+        r1 = r0 + Δε / derivative(ε, r0)
+        isinf(r1) && (r1 = Δε)
+
+        radius[i] = secant_method(r -> ε(r) - E, r0, r1, maxiter; atol)
+        if isodd(i)
+            corners[i÷2 + 1, :] = map(θ -> SVector{2, Float64}(radius[i]*cos(θ), radius[i]*sin(θ)), LinRange(0, 2π, n_angles))
+        else
+            k[i÷2, :] = map(θ -> SVector{2, Float64}(radius[i]*cos(θ), radius[i]*sin(θ)), (Δθ/2):Δθ:2π)
+            for j in 1:n_angles-1
+                ref_index = (j - 1) * n_levels + (i÷2)
+                corner_indices[i÷2, j] = [
+                    ref_index, 
+                    ref_index + 1,
+                    ref_index + n_levels + 1,
+                    ref_index + n_levels,
+                    ]
+            end
+        end
+    end
+
+    v = map(x -> derivative(ε, norm(x)) * x / norm(x), k)
+
+    J = zeros(Float64, 2, 2)
+    patches = Matrix{Patch}(undef, n_levels-1, n_angles-1)
+    for i in 1:size(patches)[1]
+        for j in 1:size(patches)[2]
+            θ = atan(k[i,j][2], k[i,j][1])
+
+            J[1,1] = cos(θ) / norm(v[i,j])
+            J[1,2] = sin(θ) / norm(v[i,j])
+            J[2,1] = - norm(k[i,j]) * sin(θ)
+            J[2,2] = norm(k[i,j]) * cos(θ)
+
+            J = inv(J)
+            J[1, :] *= 2/Δε
+            J[2, :] *= 2/Δθ
+
+            patches[i, j] = Patch(
+                ε(norm(k[i,j])),   
+                k[i,j], 
+                v[i,j],
+                Δε,
+                0.5 * Δθ * (radius[2*i - 1]^2 - radius[2*i + 1]^2),
+                inv(J),
+                1 / abs(det(J)),
+                1
+            )
+        end
+    end
+
+    return Mesh(vec(patches), vec(corners), vec(corner_indices))
+end
 
 end
