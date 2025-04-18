820148cc3508786468da112e66b5cb9e613d5242
Davis <dthuillier@gmail.com>
2025-04-18 11:28:54 -0700

    Defined kz=0 slice dispersion for Nd-LSCO

--- scripts/materials/Nd-LSCO.jl
+++ scripts/materials/Nd-LSCO.jl
@@ -7,8 +7,13 @@ const tp::Float64 = - 0.1364 # ± 0.0005
 const tpp::Float64 = 0.0682 # ± 0.0005
 const tz::Float64 = 0.0651 # ± 0.0005
 
-function ε(k::AbstractVector)
+function ε_full(k::AbstractVector)
     return - μ - 2.0 * (cos(2pi*k[1]) + cos(2pi*k[2])) - 4 * tp * cos(2pi*k[1])*cos(2pi*k[2]) - 2 * tpp * (cos(4pi * k[1]) + cos(4pi * k[2])) - 2 * tz * cos(pi*k[1]) * cos(pi*k[2]) * cos(pi*k[3]) * (cos(2pi*k[1]) - cos(2pi*k[2]))^2
+end
+
+## k_z = 0 slice
+function ε(k::AbstractVector)
+    return - μ - 2.0 * (cos(2pi*k[1]) + cos(2pi*k[2])) - 4 * tp * cos(2pi*k[1])*cos(2pi*k[2]) - 2 * tpp * (cos(4pi * k[1]) + cos(4pi * k[2])) - 2 * tz * cos(pi*k[1]) * cos(pi*k[2]) * (cos(2pi*k[1]) - cos(2pi*k[2]))^2
 end
 
 bands = [ε]
