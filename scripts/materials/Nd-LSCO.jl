6671cd032c71f085685482d4651ea15b3e98c505
Davis <dthuillier@gmail.com>
2025-04-22 11:39:18 -0700

    Added real lattices constants to Nd-LSCO material file

--- scripts/materials/Nd-LSCO.jl
+++ scripts/materials/Nd-LSCO.jl
@@ -6,6 +6,8 @@ const μ::Float64 = -0.8243 # ± 0.0005
 const tp::Float64 = - 0.1364 # ± 0.0005
 const tpp::Float64 = 0.0682 # ± 0.0005
 const tz::Float64 = 0.0651 # ± 0.0005
+const a::Float64 = 3.75e-10
+const c::Float64 = 13.2e-10
 
 function ε_full(k::AbstractVector)
     return - μ - 2.0 * (cos(2pi*k[1]) + cos(2pi*k[2])) - 4 * tp * cos(2pi*k[1])*cos(2pi*k[2]) - 2 * tpp * (cos(4pi * k[1]) + cos(4pi * k[2])) - 2 * tz * cos(pi*k[1]) * cos(pi*k[2]) * cos(pi*k[3]) * (cos(2pi*k[1]) - cos(2pi*k[2]))^2
