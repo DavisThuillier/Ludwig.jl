d19c438fbd8f1afc4f44fef121d312ddd36c39b4
Davis <dthuillier@gmail.com>
2025-04-30 12:50:23 -0700

    Dropped tz term entirely in dispersion to obtain dispersion which is 2pi periodic.

--- scripts/materials/Nd-LSCO.jl
+++ scripts/materials/Nd-LSCO.jl
@@ -1,6 +1,6 @@
 const material::String = "Nd-LSCO"
 # Tight binding parameters from arXiv:2011.13054
-const t::Float64 = 0.160 # ± 30 eV
+const t::Float64 = 0.160 # ± 30 meV
 # In units of t
 const μ::Float64 = -0.8243 # ± 0.0005 
 const tp::Float64 = - 0.1364 # ± 0.0005
@@ -13,9 +13,6 @@ function ε_full(k::AbstractVector)
     return - μ - 2.0 * (cos(2pi*k[1]) + cos(2pi*k[2])) - 4 * tp * cos(2pi*k[1])*cos(2pi*k[2]) - 2 * tpp * (cos(4pi * k[1]) + cos(4pi * k[2])) - 2 * tz * cos(pi*k[1]) * cos(pi*k[2]) * cos(pi*k[3]) * (cos(2pi*k[1]) - cos(2pi*k[2]))^2
 end
 
-## k_z = 0 slice
 function ε(k::AbstractVector)
-    return - μ - 2.0 * (cos(2pi*k[1]) + cos(2pi*k[2])) - 4 * tp * cos(2pi*k[1])*cos(2pi*k[2]) - 2 * tpp * (cos(4pi * k[1]) + cos(4pi * k[2])) - 2 * tz * cos(pi*k[1]) * cos(pi*k[2]) * (cos(2pi*k[1]) - cos(2pi*k[2]))^2
+    return - μ - 2.0 * (cos(2pi*k[1]) + cos(2pi*k[2])) - 4 * tp * cos(2pi*k[1])*cos(2pi*k[2]) - 2 * tpp * (cos(4pi * k[1]) + cos(4pi * k[2]))
 end
-
-bands = [ε]
