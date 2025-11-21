632ea7390b10e83de792b3eb538ce7ae9adbe292
Davis <dthuillier@gmail.com>
2025-11-21 09:57:59 -0800

    Restored KaTeX defaults for docs

--- docs/make.jl
+++ docs/make.jl
@@ -12,17 +12,17 @@ makedocs(
         "Collision Operator" => "collision_operator.md",
         # "Example" => "example.md"
     ],
-    format = Documenter.HTML(; mathengine=
-        Documenter.KaTeX(
-            Dict(:delimiters => [
-                Dict(:left => raw"$",   :right => raw"$",   display => false),
-                Dict(:left => raw"$$",  :right => raw"$$",  display => true),
-                Dict(:left => raw"\[",  :right => raw"\]",  display => true),
-                ],
-                :macros => Dict(),
-            )
-        )
-    )
+    # format = Documenter.HTML(; mathengine=
+    #     Documenter.KaTeX(
+    #         Dict(:delimiters => [
+    #             Dict(:left => raw"$",   :right => raw"$",   display => false),
+    #             Dict(:left => raw"$$",  :right => raw"$$",  display => true),
+    #             Dict(:left => raw"\[",  :right => raw"\]",  display => true),
+    #             ],
+    #             :macros => Dict(),
+    #         )
+    #     )
+    # )
 )
 
 deploydocs(
