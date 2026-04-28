85a78db2392c1f347ab73aa70531e48ae6f02fee
DavisThuillier <dsthuillier@gmail.com>
2026-04-28 13:33:10 -0700

    Removed incomplete Hall resistivity section.

--- src/properties.jl
+++ src/properties.jl
@@ -279,21 +279,3 @@ function η_lifetime(L, D, E, dV, T)
 
     return τ_eff * hbar
 end
-
-###
-### Hall Resistivity (FIXME: incomplete)
-###
-
-delta(i,j) = i == j ? 1.0 : 0.0
-delta(v::Vector, i::Int, j::Int) = v[i] == v[j] ? 1.0 : 0.0
-
-function momentum_derivative(n_ε, n_θ, n_bands, k, v, E)
-    D = Matrix{Float64}(undef, length(k), length(k))
-    for _j in eachindex(k)
-        εi, θi 
-    end
-end
-
-function hall_coefficient(L, k, v, E, dV, T; kwargs)
-    momentum_derivative(kwargs[:n_ε], kwargs[:n_θ], kwargs[:n_bands], k, v, E)
-end
