59995e6a599c84ed5879e8b82b072bfcdc56951d
DavisThuillier <dsthuillier@gmail.com>
2026-05-05 09:19:41 -0700

    Included separated files into main module

--- src/Ludwig.jl
+++ src/Ludwig.jl
@@ -4,4 +4,12 @@ module Ludwig
     using LinearAlgebra
     using StaticArrays
 
+    include(joinpath("crystal", "polytope.jl"))
+    include(joinpath("utilities", "convex_hull.jl"))
+    include(joinpath("crystal", "brillouin_zone.jl"))
+    include(joinpath("crystal", "irreducible_brillouin_zone.jl"))
+    include(joinpath("crystal", "lattice.jl"))
+    include(joinpath("crystal", "point_group.jl"))
+    include(joinpath("crystal", "crystal.jl"))
+
 end
