504537ca9d636437373ffaf8bd5cfeb491a34c9a
DavisThuillier <dsthuillier@gmail.com>
2026-05-05 11:42:37 -0700

    Added proper IBZ creation to crystal constructor

--- src/crystal/crystal.jl
+++ src/crystal/crystal.jl
@@ -2,17 +2,18 @@ struct Crystal{D, T, L}
     lattice::Lattice{D, T, L}
     point_group::PointGroup{D, T, L}
     irreducible_brillouin_zone::IrreducibleBrillouinZone{D, T}
-    # function Crystal(lattice::Latice{D, T, L}, point_group::PointGroup{D}) where {D,T,L}
-    #     new{D,T,L}(lattice, point_group, IrreducibleBrillouinZone(l, pg))
-    # end
+end
+
+function Crystal(lattice::Lattice{D, T, L}, pg::PointGroup{D, T, L}) where {D, T, L}
+    return Crystal{D, T, L}(lattice, pg, IrreducibleBrillouinZone(lattice, pg))
+end
+
+function Crystal(lattice::Lattice{D, T, L},
+                 generators::AbstractVector{<:AbstractMatrix}) where {D, T, L}
+    return Crystal(lattice, PointGroup(generators, lattice))
 end
 
 lattice(c::Crystal) = c.lattice
 point_group(c::Crystal) = c.point_group
 irreducible_brillouin_zone(c::Crystal) = c.irreducible_brillouin_zone
-brillouin_zone(c::Crystal) = brillouin_zone(lattice(c))
-
-function Crystal(lattice::Lattice{D,T,L}, generators::AbstractVector{AbstractMatrix{T}}) where {D,T,L}
-    point_group = PointGroup(generators, lattice)
-    return Crystal(lattice, point_group)
-end
+brillouin_zone(c::Crystal) = c.lattice.brillouin_zone
