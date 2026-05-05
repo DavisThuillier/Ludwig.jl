74315f3b36adaf1c13d4dfda350d71310971eda9
DavisThuillier <dsthuillier@gmail.com>
2026-05-05 11:41:52 -0700

    Added common polytope plotting to extension for both BZs and IBZ

--- ext/LudwigMakieExt.jl
+++ ext/LudwigMakieExt.jl
@@ -4,41 +4,101 @@ using Ludwig
 import Makie
 
 ###
-### plot_brillouin_zone
+### plot_polytope! / plot_polytope (internal): render any AbstractConvexPolytope
 ###
 
-function Ludwig.plot_brillouin_zone(
-    bz::Ludwig.BrillouinZone{3};
-    axis=(;),
-    figure=(;),
+function plot_polytope!(
+    ax,
+    p::Ludwig.AbstractConvexPolytope{2};
     facecolor=(:steelblue, 0.35),
     edgecolor=:black,
     edgewidth=2,
-    vertexcolor=:black,
-    vertexsize=10,
 )
-    fig = Makie.Figure(; figure...)
-    ax = Makie.Axis3(fig[1, 1]; aspect=:equal, axis...)
+    # Sort vertices CCW around the centroid so they form the polygon boundary.
+    c = sum(p.vertices) / length(p.vertices)
+    angles = [atan(v[2] - c[2], v[1] - c[1]) for v in p.vertices]
+    points = [Makie.Point2f(p.vertices[i]) for i in sortperm(angles)]
 
-    points = [Makie.Point3f(v) for v in bz.vertices]
+    return Makie.poly!(ax, Makie.Polygon(points);
+        color=facecolor,
+        strokecolor=edgecolor,
+        strokewidth=edgewidth,
+    )
+end
+
+function plot_polytope!(
+    ax,
+    p::Ludwig.AbstractConvexPolytope{3};
+    facecolor=(:steelblue, 0.35),
+    edgecolor=:black,
+    edgewidth=2,
+)
+    points = [Makie.Point3f(v) for v in p.vertices]
 
     tris = NTuple{3, Int}[]
-    for f in bz.facets
+    for f in p.facets
         for i in 2:length(f.vertices) - 1
             push!(tris, (f.vertices[1], f.vertices[i], f.vertices[i + 1]))
         end
     end
     faces = [Makie.GeometryBasics.TriangleFace(t...) for t in tris]
     msh = Makie.GeometryBasics.Mesh(points, faces)
-    p = Makie.mesh!(ax, msh; color=facecolor, transparency=true)
+    plt = Makie.mesh!(ax, msh; color=facecolor, transparency=true)
 
     segs = Makie.Point3f[]
-    for (i, j) in bz.ridges
+    for (i, j) in p.ridges
         push!(segs, points[i], points[j])
     end
     Makie.linesegments!(ax, segs; color=edgecolor, linewidth=edgewidth)
 
-    return Makie.FigureAxisPlot(fig, ax, p)
+    return plt
 end
+
+function plot_polytope(
+    p::Ludwig.AbstractConvexPolytope{2};
+    axis=(;),
+    figure=(;),
+    kwargs...,
+)
+    fig = Makie.Figure(; figure...)
+    ax = Makie.Axis(fig[1, 1];
+        aspect=Makie.DataAspect(),
+        xgridvisible=false,
+        ygridvisible=false,
+        axis...,
+    )
+    plt = plot_polytope!(ax, p; kwargs...)
+    return Makie.FigureAxisPlot(fig, ax, plt)
+end
+
+function plot_polytope(
+    p::Ludwig.AbstractConvexPolytope{3};
+    axis=(;),
+    figure=(;),
+    kwargs...,
+)
+    fig = Makie.Figure(; figure...)
+    ax = Makie.Axis3(fig[1, 1]; aspect=:equal, axis...)
+    plt = plot_polytope!(ax, p; kwargs...)
+    return Makie.FigureAxisPlot(fig, ax, plt)
+end
+
+###
+### Public entry points
+###
+
+Ludwig.plot_brillouin_zone(bz::Ludwig.BrillouinZone; kwargs...) =
+    plot_polytope(bz; kwargs...)
+
+Ludwig.plot_brillouin_zone!(ax, bz::Ludwig.BrillouinZone; kwargs...) =
+    plot_polytope!(ax, bz; kwargs...)
+
+Ludwig.plot_irreducible_brillouin_zone(
+    ibz::Ludwig.IrreducibleBrillouinZone; kwargs...
+) = plot_polytope(ibz; kwargs...)
+
+Ludwig.plot_irreducible_brillouin_zone!(
+    ax, ibz::Ludwig.IrreducibleBrillouinZone; kwargs...
+) = plot_polytope!(ax, ibz; kwargs...)
 
 end # module LudwigMakieExt
--- src/Ludwig.jl
+++ src/Ludwig.jl
@@ -7,24 +7,45 @@ module Ludwig
     include(joinpath("crystal", "polytope.jl"))
     include(joinpath("utilities", "convex_hull.jl"))
     include(joinpath("crystal", "brillouin_zone.jl"))
-    include(joinpath("crystal", "irreducible_brillouin_zone.jl"))
     include(joinpath("crystal", "lattice.jl"))
     include(joinpath("crystal", "point_group.jl"))
+    include(joinpath("crystal", "irreducible_brillouin_zone.jl"))
     include(joinpath("crystal", "crystal.jl"))
 
     export Lattice
     export reciprocal_lattice_vectors
 
     export BrillouinZone
+    export PointGroup
+    export IrreducibleBrillouinZone
+    export Crystal
 
     """
-        plot_brillouin_zone(bz::BrillouinZone{3}; <keyword arguments>)
+        plot_brillouin_zone(bz::BrillouinZone{D}; <keyword arguments>)
+        plot_brillouin_zone!(ax, bz::BrillouinZone{D}; <keyword arguments>)
 
-    Plot a 3D Brillouin zone in a new figure with an `Axis3`. Method provided by
-    the `LudwigMakieExt` extension; activates when a Makie backend (e.g.
-    `GLMakie`, `CairoMakie`, `WGLMakie`) is loaded.
+    Plot a Brillouin zone: a polygon for `D = 2`, a polyhedral mesh for
+    `D = 3`. The non-mutating form creates a new figure with an `Axis` (2D)
+    or `Axis3` (3D); the mutating form draws into an existing `ax`. Methods
+    provided by the `LudwigMakieExt` extension; activate when a Makie backend
+    (e.g. `GLMakie`, `CairoMakie`, `WGLMakie`) is loaded.
     """
     function plot_brillouin_zone end
-    export plot_brillouin_zone
+    function plot_brillouin_zone! end
+    export plot_brillouin_zone, plot_brillouin_zone!
+
+    """
+        plot_irreducible_brillouin_zone(ibz::IrreducibleBrillouinZone{D}; <keyword arguments>)
+        plot_irreducible_brillouin_zone!(ax, ibz::IrreducibleBrillouinZone{D}; <keyword arguments>)
+
+    Plot an irreducible Brillouin zone: a polygon for `D = 2`, a polyhedral
+    mesh for `D = 3`. The non-mutating form creates a new figure with an
+    `Axis` (2D) or `Axis3` (3D); the mutating form draws into an existing
+    `ax`. Methods provided by the `LudwigMakieExt` extension; activate when a
+    Makie backend (e.g. `GLMakie`, `CairoMakie`, `WGLMakie`) is loaded.
+    """
+    function plot_irreducible_brillouin_zone end
+    function plot_irreducible_brillouin_zone! end
+    export plot_irreducible_brillouin_zone, plot_irreducible_brillouin_zone!
 
 end
