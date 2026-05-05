bea323434e5c9005297476a0a782e4a1bb50ac55
DavisThuillier <dsthuillier@gmail.com>
2026-05-05 10:03:51 -0700

    Exported plotting extension

--- src/Ludwig.jl
+++ src/Ludwig.jl
@@ -12,4 +12,19 @@ module Ludwig
     include(joinpath("crystal", "point_group.jl"))
     include(joinpath("crystal", "crystal.jl"))
 
+    export Lattice
+    export reciprocal_lattice_vectors
+
+    export BrillouinZone
+
+    """
+        plot_brillouin_zone(bz::BrillouinZone{3}; <keyword arguments>)
+
+    Plot a 3D Brillouin zone in a new figure with an `Axis3`. Method provided by
+    the `LudwigMakieExt` extension; activates when a Makie backend (e.g.
+    `GLMakie`, `CairoMakie`, `WGLMakie`) is loaded.
+    """
+    function plot_brillouin_zone end
+    export plot_brillouin_zone
+
 end
